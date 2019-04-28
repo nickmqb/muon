b695441d55b6881e67f99f0bf44a3acd9c6dcf9c
nickmqb <48037281+nickmqb@users.noreply.github.com>
2019-04-28 18:18:59 +0200

    Remove custom msvc specific build script

--- compiler/rcc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy /y mu.exe mu_copy.exe >nul
-mu_copy.exe --args mu.args --args msvc_debug.args --print-stats
-if %errorlevel% neq 0 exit /b
-copy /y mu.exe mu_copy.exe >nul
-mu_copy.exe --args mu.args --args msvc_debug.args --print-stats
